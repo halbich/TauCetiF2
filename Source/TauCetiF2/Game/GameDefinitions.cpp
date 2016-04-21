56f4614fc7650541ec2a6f1391c39c95ff26f0f9
Pavel Halbich <halbich.p@hotmail.com>
2016-04-21 18:19:35 +0200

    landscape

--- Source/TauCetiF2/Game/GameDefinitions.cpp
+++ Source/TauCetiF2/Game/GameDefinitions.cpp
@@ -3,7 +3,7 @@
 #include "TauCetiF2.h"
 #include "GameDefinitions.h"
 
-const FVector GameDefinitions::WorldBorders(500, 500, 200);
+const FVector GameDefinitions::WorldBorders(50000, 50000, 10000);
 const FVector GameDefinitions::WorldCenterMapping(GameDefinitions::WorldBorders.X / 2, GameDefinitions::WorldBorders.Y / 2, 0);
 const int32 GameDefinitions::CubeMinSize(20);
 const FRotator GameDefinitions::WorldGrid(90, 90, 90);
