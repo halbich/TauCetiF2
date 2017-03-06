51377b439880ec628220a58696eb128d3a01a839
Pavel Halbich <halbich.p@hotmail.com>
2017-03-06 18:24:39 +0100

    sync

---
+++ Source/TauCetiF2/World/WeatherController.cpp
@@ -0,0 +1,28 @@
+
+
+#include "TauCetiF2.h"
+#include "WeatherController.h"
+
+
+// Sets default values
+AWeatherController::AWeatherController()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AWeatherController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AWeatherController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
